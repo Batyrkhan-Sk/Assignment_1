b0ddf0a26608c456cc3a3c8a675cdfa7d61d7f08
Batyr <bshekish@gmail.com>
2023-12-24 15:39:32 +0600

    added classes, point and shape

--- src/models/Point.java
+++ src/models/Point.java
@@ -1,6 +1,32 @@
 package models;
 
+import java.lang.Math;
+
 public class Point {
     private double x;
     private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double distance(Point another) {
+        return Math.sqrt(
+                Math.pow(this.getX() - another.getX(), 2) +
+                        Math.pow(this.getY() - another.getY(), 2)
+        );
+    }
+
+    public String toString() {
+        return "Point at x: " + String.valueOf(x) + " y: " + String.valueOf(y) + '\n';
+    }
 }
--- src/models/Shape.java
+++ src/models/Shape.java
@@ -1,7 +1,41 @@
 package models;
+
+import java.util.ArrayList;
+
 public class Shape {
+    private ArrayList<Point> points = new ArrayList<Point>();
 
-
+    public void addPoint(Point a) {
+        points.add(a);
     }
 
+    public double calculatePerimeter() {
+        double perimeter = 0;
+
+        for (int i = 0; i < points.size(); i++) {
+            Point currentPoint = points.get(i);
+            Point nextPoint = points.get((i + 1) % points.size());
+            perimeter += currentPoint.distance(nextPoint);
+        }
+
+        return perimeter;
+    }
+
+    public double getLongest() {
+        double longestSide = 0;
+
+        for (int i = 0; i < points.size(); i++) {
+            double sideLength = points.get(i).distance(points.get((i + 1) % points.size()));
+            if (sideLength > longestSide) {
+                longestSide = sideLength;
+            }
+        }
+
+        return longestSide;
+    }
+
+    public double getAverageSide() {
+        double totalSideLength = calculatePerimeter();
+        return totalSideLength / points.size();
+    }
 }
